98353d78aa6caac7b121883805ee31c856868bbe
Marcos Pozzo <pozzomarc@gmail.com>
2023-09-03 04:20:35 -0300

    professionals view work in progress

--- app/profesionales/page.tsx
+++ app/profesionales/page.tsx
@@ -1,14 +1,26 @@
-import { ProfessionalProps } from "@/types";
+import { ProfessionalProps, SkillProps } from "@/types";
 import { getData } from "../getData";
 import ProfessionalCard from "@/components/ProfessionalCard";
 
 const Profesionales = async () => {
-  const data = await getData("/professionals");
+  const skills = await getData("/skills");
+  const professionals = await getData("/professionals");
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full">
-      {data.map((professional: ProfessionalProps) => (
-        <ProfessionalCard key={professional.id} professional={professional} />
+    <div>
+      {skills.map((skill: SkillProps) => (
+        <div>
+          <h1>{skill.name}</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full">
+            {skill.professionals.map((id: number) => {
+              const index = professionals.findIndex(
+                (professional: { id: number }) => professional.id === id
+              );
+              <ProfessionalCard key={id} professional={professionals[index]} />;
+              return null;
+            })}
+          </div>
+        </div>
       ))}
     </div>
   );
