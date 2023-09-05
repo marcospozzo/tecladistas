e4de802c690265751b30c264f759e0bdbd243dfa
Marcos Pozzo <pozzomarc@gmail.com>
2023-09-05 03:50:05 -0300

    navbar styling

--- components/ProfessionalCard.tsx
+++ components/ProfessionalCard.tsx
@@ -10,13 +10,6 @@ const ProfessionalCard = ({
   return (
     <div className="flex items-center justify-between space-x-4 m-2 p-4 bg-slate-300 rounded-xl">
       <div className="flex flex-row items-center space-x-4">
-        <Image
-          className="profile-picture"
-          src={"/profile-picture-placeholder.jpg"}
-          alt={`${professional.firstName}'s profile picture`}
-          width={40}
-          height={40}
-        />
         <p>
           {professional.firstName}{" "}
           {professional.nickname && `"${professional.nickname}"`}{" "}
