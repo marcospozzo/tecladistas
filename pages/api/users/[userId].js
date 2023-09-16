ed577da1bb68a2100d8b20ba8fb04d3c9e5aa4a4
Marcos Pozzo <pozzomarc@gmail.com>
2023-09-15 22:40:22 -0300

    mock api added with mock data. not for using it in the project, but to keep it stored on github

---
+++ pages/api/users/[userId].js
@@ -0,0 +1,10 @@
+import { users } from "../users";
+
+export default function (req, res) {
+  const { userId } = req.query;
+  const user = users.find((user) => user.id === parseInt(userId));
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  res.json(user);
+}
