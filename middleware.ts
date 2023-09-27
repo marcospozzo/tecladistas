5630e99c30119d34363a561d6683dc6e5e7f79fa
Marcos Pozzo <pozzomarc@gmail.com>
2023-09-27 02:14:14 -0300

    token cookie __Secure added if env is prod

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { cookieName } from "./utils/utils";
 // export { default } from "next-auth/middleware";
 
 export const config = {
@@ -18,7 +19,7 @@ export function middleware(request: NextRequest) {
   if (
     request.nextUrl.pathname !== "/entrar" &&
     request.nextUrl.pathname !== "/registrarse" &&
-    !request.cookies.has("next-auth.session-token")
+    !request.cookies.has(cookieName)
   ) {
     const signinUrl = new URL("/entrar", request.url);
     signinUrl.searchParams.set("callbackUrl", request.nextUrl.pathname);
@@ -27,7 +28,7 @@ export function middleware(request: NextRequest) {
   if (
     (request.nextUrl.pathname === "/entrar" ||
       request.nextUrl.pathname === "/registrarse") &&
-    request.cookies.has("next-auth.session-token")
+    request.cookies.has(cookieName)
   ) {
     return NextResponse.redirect(new URL("/clasificados", request.url));
   }
