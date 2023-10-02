8c713e152a3e1b044d151391d0dbb21b07c95ff8
Marcos Pozzo <pozzomarc@gmail.com>
2023-10-02 02:37:03 -0300

    users can now delete their own products, text changes

---
+++ components/DeleteProductButton.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+const DeleteProductButton = ({ id }: { id: string | undefined }) => {
+  const router = useRouter();
+  const [isConfirmed, setIsConfirmed] = useState(false);
+
+  const handleDelete = () => {
+    setIsConfirmed(true);
+  };
+
+  const handleConfirm = async () => {
+    try {
+      const promise = axios.delete(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/products/${id}`,
+        {
+          withCredentials: true,
+        }
+      );
+      toast.promise(promise, {
+        pending: "Eliminando...",
+        error: {
+          render({
+            data,
+          }: {
+            data?: { response?: { data?: { error?: string } } };
+          }) {
+            console.log({ data });
+            return data?.response?.data?.error ?? "Error";
+          },
+        },
+      });
+      await promise;
+      router.push("/clasificados");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col text-lg space-x-1">
+      {!isConfirmed && (
+        <button
+          className="flex justify-center delete-button space-x-2 my-2"
+          onClick={handleDelete}
+        >
+          <h3 className="text-base">Borrar publicación</h3>
+        </button>
+      )}
+      {isConfirmed && (
+        <button
+          className="flex justify-center submit-button space-x-2 my-2 w-fit self-center"
+          onClick={handleConfirm}
+        >
+          <h3 className="text-base">Confirmar</h3>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default DeleteProductButton;
