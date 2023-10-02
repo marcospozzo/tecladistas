63d16f1287115c1ab98093ae48c6bebb28dfba4e
Marcos Pozzo <pozzomarc@gmail.com>
2023-10-02 03:56:18 -0300

    footer, contact form, bug fix

--- app/clasificados/page.tsx
+++ app/clasificados/page.tsx
@@ -5,15 +5,24 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 
 const Classified = async () => {
-  const data = await getProducts();
+  const products = await getProducts();
   const session = await getServerSession(authOptions);
   const userId = session?.user.id;
+  const currentUserProducts = products.filter(
+    (product) => product.userId === userId
+  );
+  const otherUsersProducts = products.filter(
+    (product) => product.userId !== userId
+  );
+  const sortedProducts = currentUserProducts.concat(otherUsersProducts);
 
   return (
     <Cards>
-      {/* hide the create button if user already has an active one */}
-      {data[0].userId !== userId && <CardNew />}
-      {data?.map((product: ProductProps) => (
+      {/* show create button if user does not have on created yet */}
+      {(products.length === 0 || currentUserProducts.length === 0) && (
+        <CardNew />
+      )}
+      {sortedProducts?.map((product: ProductProps) => (
         <ProductCard
           key={product._id}
           product={product}
--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,20 +1,70 @@
+"use client";
+
 import { contactSubjects } from "@/utils/utils";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+import { toast } from "react-toastify";
 
 const Contact = () => {
+  const router = useRouter();
+  const [data, setData] = useState({
+    subject: contactSubjects.comment,
+    message: "",
+  });
+
+  const handleOnChange = (event: { target: { name: any; value: any } }) => {
+    if (event.target) {
+      setData({
+        ...data,
+        [event.target.name]: event.target.value,
+      });
+    }
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const promise = axios.post(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/contact-form`,
+        data,
+        {
+          withCredentials: true,
+        }
+      );
+      toast.promise(promise, {
+        pending: "Enviando...",
+        success: "Enviado",
+        error: {
+          render({
+            data,
+          }: {
+            data?: { response?: { data?: { error?: string } } };
+          }) {
+            console.log({ data });
+            return data?.response?.data?.error ?? "Error";
+          },
+        },
+      });
+      await promise;
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <form className="wide-form" action="#" method="post">
+    <form className="wide-form" onSubmit={handleSubmit}>
       <h1 className="form-title">Contacto</h1>
-      <input type="text" id="name" name="name" placeholder="Nombre" required />
 
-      <input
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Email"
+      <select
+        className="bg-white"
+        id="subject"
+        name="subject"
+        onChange={handleOnChange} // Add this line
         required
-      />
-
-      <select className="bg-white" id="subject" name="subject" required>
+      >
         <option value={contactSubjects.comment}>
           {contactSubjects.comment}
         </option>
@@ -32,6 +82,7 @@ const Contact = () => {
         id="message"
         name="message"
         placeholder="Mensaje"
+        onChange={handleOnChange}
         required
       ></textarea>
 
@@ -42,7 +93,7 @@ const Contact = () => {
         type="submit"
         value="Enviar"
       >
-        <h3 className="text-xl">Enviar</h3>
+        <h3 className="text-xl">Enviar mensaje</h3>
       </button>
     </form>
   );
