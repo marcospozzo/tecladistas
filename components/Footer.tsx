017c5f229d88697d2153d2361f5979ced2978696
Marcos Pozzo <pozzomarc@gmail.com>
2023-10-04 20:24:52 -0300

    working fix applied to footer as well

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import { CONTACT, EMAIL } from "@/utils/constants";
+import { useSession } from "next-auth/react";
 
-const Footer = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
+const Footer = () => {
+  const { status } = useSession();
+  const isLoggedIn = status === "authenticated";
+
   return (
     <footer className="flex justify-center h-14 bg-gray-400">
       <nav className="flex flex-row">
