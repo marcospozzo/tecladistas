6d54b812ef2028ecd36b92d5f44b9104351a4ea7
Marcos Pozzo <pozzomarc@gmail.com>
2023-10-08 20:08:13 -0300

    old import removed

--- app/entrar/page.tsx
+++ app/entrar/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { login } from "@/utils/axios";
 import Link from "next/link";
 import { useState } from "react";
 import { toast } from "react-toastify";
