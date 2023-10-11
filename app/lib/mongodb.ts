1a960ab12c4b92b0c34a5359f0f71f78fc2f4764
Marcos Pozzo <pozzomarc@gmail.com>
2023-10-11 16:17:22 -0300

    mongodb connection retry logic implemented due to bug

--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -21,19 +21,40 @@ const options = {
 let client;
 let clientPromise: Promise<MongoClient>;
 
+const maxRetries = 3; // You can adjust the number of retries as needed
+let currentRetries = 0;
+
+function connectWithRetry(): Promise<MongoClient> {
+  return clientPromise.catch((error) => {
+    if (currentRetries < maxRetries) {
+      currentRetries++;
+      console.log(`Connection failed. Retrying (Attempt ${currentRetries})...`);
+      return connectWithRetry(); // Recursive call to retry
+    }
+    throw error; // If max retries are reached, throw the error
+  });
+}
+
 if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    let client = new MongoClient(uri, options);
+    global._mongoClientPromise = connectWithRetry(); // Retry logic added here
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  let client = new MongoClient(uri, options);
+  clientPromise = connectWithRetry(); // Retry logic added here
 }
+
+// Usage:
+connectWithRetry()
+  .then((client) => {
+    console.log("Connected to MongoDB successfully.");
+    // You can use the 'client' here for database operations
+  })
+  .catch((error) => {
+    console.error("Failed to connect after retries:", error);
+  });
 
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
