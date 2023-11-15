5193a1e5efadad06648712393f0dc7455d72c6f9
Marcos Pozzo <pozzomarc@gmail.com>
2023-11-15 16:02:33 -0300

    ids added to professionals page to allow #id on url

--- app/profesionales/page.tsx
+++ app/profesionales/page.tsx
@@ -1,6 +1,6 @@
 import { ProfessionalProps } from "@/types";
 import { ProfessionalCard } from "@/components";
-import { pageTitles, skillsTranslations } from "@/utils/utils";
+import { pageTitles, skillsPageIds, skillsTranslations } from "@/utils/utils";
 import { skills } from "@/utils/utils";
 import { getProfessionals } from "@/utils/axios";
 import { Metadata } from "next";
@@ -15,7 +15,7 @@ const Professionals = async () => {
   return (
     <div>
       {skills.map((skill: string) => (
-        <div key={skill} className="mb-14">
+        <div key={skill} className="mb-14" id={skillsPageIds[skill]}>
           <h1 className="mb-4">{skillsTranslations[skill]}</h1>
           <div className="professionals px-6 sm:px-12">
             {professionals
