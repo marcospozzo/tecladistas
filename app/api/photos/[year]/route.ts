b7e7fb8ac5b6297d5163ed717ec5d953905c0ba3
Marcos Pozzo <pozzomarc@gmail.com>
2025-10-03 13:08:10 -0300

    refactor: simplify photos API route by removing cookie handling and updating headers

--- app/api/photos/[year]/route.ts
+++ app/api/photos/[year]/route.ts
@@ -1,22 +1,13 @@
-import { cookieName } from "@/utils/utils";
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export async function GET(
-  _request: Request,
-  { params }: { params: { year: string } }
-) {
-  const cookieStore = cookies();
-  const cookie = cookieStore.get(cookieName);
-
+export async function GET({ params }: { params: { year: string } }) {
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_BASE_URL}/photos/${params.year}`,
       {
         headers: {
-          cookie: `${cookie?.name}=${cookie?.value}`,
           authorization: `${process.env.NEXT_SECRET}`,
-          "accept-encoding": "gzip, deflate",
+          cache: "no-store",
         },
       }
     );
