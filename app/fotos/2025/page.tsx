9114a675d4c9e66d7c47767ed8527748388943e3
Marcos Pozzo <mpozzo@dataxstream.com>
2025-10-05 13:03:00 -0300

    feat: add photo gallery page for 2025

---
+++ app/fotos/2025/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { constants } from "@/utils/utils";
+import axios from "axios";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { FaCamera } from "react-icons/fa6";
+import ImageGallery, { ReactImageGalleryItem } from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+
+export default function Page() {
+  const [photos, setPhotos] = useState<ReactImageGalleryItem[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/api/photos/2023`);
+        setPhotos(res.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div className="flex justify-center gap-x-2 mb-4">
+        <FaCamera className="self-center" />
+        <Link
+          className="underline"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={constants.PHOTOGRAPHERS_INSTAGRAM_URL}
+        >
+          {constants.PHOTOGRAPHERS_INSTAGRAM}
+        </Link>
+      </div>
+      <ImageGallery
+        showPlayButton={false}
+        thumbnailPosition="top"
+        showIndex={true}
+        lazyLoad={true}
+        items={photos}
+      />
+    </>
+  );
+}
