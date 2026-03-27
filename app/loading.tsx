96231d008034a2e34a01b20f50879cc502952d63
Marcos Pozzo <pozzomarc@gmail.com>
2026-03-27 16:19:51 +0100

    feat: add synth knob as loading spinner

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,18 @@
-"use client";
+import Image from "next/image";
 
 const Loading = () => {
-  return <div className="flex justify-center">Cargando...</div>;
+  return (
+    <div className="flex min-h-[70vh] w-full items-center justify-center">
+      <Image
+        alt="Synth knob cargando"
+        className="h-20 w-20 animate-[spin_2.5s_linear_infinite]"
+        height={80}
+        priority
+        src="/logo.svg"
+        width={80}
+      />
+    </div>
+  );
 };
 
 export default Loading;
